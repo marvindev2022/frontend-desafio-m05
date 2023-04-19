16633f04c8670ee9b026f9ded718434841e3aaa2
Marcus Roza <marcusroza@pop-os.localdomain>
2023-04-18 22:18:19 -0300

    teste deploy front

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,16 @@
-import {Navigate, Outlet, Route, Routes} from "react-router-dom";
-import {ToastContainer} from "react-toastify";
+import { Navigate, Outlet, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
 import Main from "./pages/Main/Main";
 import Signin from "./pages/Signin/Signin";
-import {getItem} from "./utils/storage";
+import { getItem } from "./utils/storage";
 import SignUp from "./pages/Signup/Signup";
 
-function ProtectedRoutes({redirectTo}) {
+function ProtectedRoutes({ redirectTo }) {
   const token = getItem("token");
+  return token ? <Outlet /> : <Navigate to={redirectTo} />;
+}
+function NotFound({ redirectTo }) {
+  const token = false;
   return token ? <Outlet /> : <Navigate to={redirectTo} />;
 }
 
@@ -21,6 +25,9 @@ function MainRoutes() {
         <Route element={<ProtectedRoutes redirectTo="/signin" />}>
           <Route path="/main" element={<Main />} />
           <Route path="*" element={<Main />} />
+        </Route>
+        <Route element={<NotFound redirectTo="/signin" />}>
+          <Route path="/" element={<Main />} />
         </Route>
       </Routes>
     </>
